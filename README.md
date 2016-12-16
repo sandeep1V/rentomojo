1837d65770597f261fb305816465db3d639cfd97
s1729v <sandeep@slicepay.in>
2016-12-16 23:33:35 +0530

    sending output hyper links to csv

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # rentomojo
 run node app.js
-Then from hit http://localhost:8081/scrape
+Then hit http://localhost:8081/scrape
 
 Things done
-1) Scraping medium.com recursively
-2) Maintaing concurrency to 5 using async library
+1) Scraping medium.com recursively.
+2) Maintaing concurrency to 5 using async library.
+3) Export to csv(output.csv) once crawling has been done.
 
 To be done:
 1) Did with async, to be done without async.
-2) exporting hyperlinks to csv
--- app.js
+++ app.js
@@ -3,36 +3,36 @@ var fs      = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
-
-function scrapePage(url) {
-    request(url, function(error, response, html){
-        if(!error){
-            // console.log(url);
-            var $ = cheerio.load(html);
-            var hyperLinks = $('a');
-            var q = require('async').queue(function (task, callback) {
-                console.log(task.url);
-                if(task.url){
-                    scrapePage(task.url);
-                }
-                callback();
-            }, 5);
+var async = require('async');
+var resultUrls = [];
 
 
-            q.drain = function() {
-                console.log('all items have been processed');
-            }
-            for(var i = 0; i<hyperLinks.length; i++){
-                // output += "\n"+$(hyperLinks[i]).attr('href');
-                q.push({url:$(hyperLinks[i]).attr('href')});
-            }
-
+var queue = async.queue(function scrapePage(url, next) {
+    if (!url || resultUrls.indexOf(url) !== -1) return next(null);
+    request(url, function(err, response, body){
+        if (err) return next(err);
+        resultUrls.push(url);
+        var $ = cheerio.load(body);
+        var hyperLinks = $('a');
+        for(var i=0; i<hyperLinks.length;i++){
+            queue.push($(hyperLinks[i]).attr('href'));
         }
-    })
-}
+        next(null);
+    });
+}, 5);
+
 app.get('/scrape', function(req, res){
-    url = 'https://medium.com';
-    scrapePage(url);
+    queue.push('https://medium.com');
+    queue.drain = function () {
+        //called at the end
+        var output = resultUrls[0];
+        for(var i=1; i<resultUrls.length;i++){
+            output += "\n"+resultUrls[i]
+        }
+        fs.writeFile('output.csv',output, function (err, resp) {
+            console.log('done');
+        })
+    }
 });
 
 app.listen('8081');
