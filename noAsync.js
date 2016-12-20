bc6adccd82ed8d3b814f888112e12b2c313d0084
s1729v <sandeep@slicepay.in>
2016-12-20 23:55:28 +0530

    without async

--- noAsync.js
+++ noAsync.js
@@ -1,90 +1,49 @@
-var express = require('express');
-var fs      = require('fs');
-var request = require('request');
+var https = require('https');
 var cheerio = require('cheerio');
-var app     = express();
-var async = require('async');
-var resultUrls = [];
-
-
-var queue = async.queue(function scrapePage(url, next) {
-    if (!url || resultUrls.indexOf(url) !== -1) return next(null);
-    request(url, function(err, response, body){
-        if (err) return next(err);
-        resultUrls.push(url);
-        var $ = cheerio.load(body);
-        var hyperLinks = $('a');
-        for(var i=0; i<hyperLinks.length;i++){
-            queue.push($(hyperLinks[i]).attr('href'));
+var totalUrl = 10;
+var currentUrl = 0;
+var resultUrl = ['https://medium.com'];
+var totalLoop = 5;
+    setConcurrentRequests(resultUrl[0]);
+    // getNextUrl(resultUrl[0]);
+function setConcurrentRequests(url) {
+    var concurrentLimit = 5;
+    getNextUrl(url);
+    function getNextUrl(){
+        if(concurrentLimit == 0){
+            getNextUrl(url);
+            return
         }
-        next(null);
-    });
-}, 5);
+        concurrentLimit--;
 
-app.get('/scrape', function(req, res){
-    queue.push('https://medium.com');
-    queue.drain = function () {
-        //called at the end
-        var output = resultUrls[0];
-        for(var i=1; i<resultUrls.length;i++){
-            output += "\n"+resultUrls[i]
+        if(currentUrl >= totalUrl){
+            console.log("nothing else to do for this worker");
+            return;
         }
-        fs.writeFile('output.csv',output, function (err, resp) {
-            console.log('done');
-        })
-    }
-});
 
-var Crawler = require("crawler");
-var url = require('url');
+        https.get(url, function(res) {
+            var pageData = "";
+            res.resume();
+            res.on('data', function (chunk) {
+                if(res.statusCode == 200){
+                    pageData +=chunk;
+                }
+            });
+            res.on('end', function() {
+                concurrentLimit++;
+                // do something with the HTML page
+                var $ = cheerio.load(pageData);
+                var hyperLinks = $('a');
+                for(var i=0; i<hyperLinks.length;i++){
+                    resultUrl.push($(hyperLinks[i]).attr('href'));
+                    console.log(resultUrl);
+                    getNextUrl($(hyperLinks[i]).attr('href'));
+                }
+                // getNextUrl(); //call the next url to fetch
+            });
 
-var c = new Crawler({
-    maxConnections : 5,
-    // This will be called for each crawled page
-    callback : function (error, res, done) {
-        if(error){
-            console.log(error);
-        }else{
-            var $ = res.$;
-            // $ is Cheerio by default
-            //a lean implementation of core jQuery designed specifically for the server
-
-            var hyperLinks = $('a');
-            for(var i=0; i<5;i++){
-                console.log($(hyperLinks[i]).attr('href'));
-                c.queue($(hyperLinks[i]).attr('href'));
-            }
-        }
-        done();
-    }
-});
-
-// Queue just one URL, with default callback
-c.queue('https://medium.com');
-
-c.on('drain', function () {
-    console.log('done') ;
-});
-// Queue a list of URLs
-// c.queue(['http://www.google.com/','http://www.yahoo.com']);
-
-// Queue URLs with custom callbacks & parameters
-// c.queue([{
-//     uri: 'http://parishackers.org/',
-//     jQuery: false,
-//
-//     // The global callback won't be called
-//     callback: function (error, res, done) {
-//         if(error){
-//             console.log(error);
-//         }else{
-//             console.log('Grabbed', res.body.length, 'bytes');
-//         }
-//         done();
-//     }
-// }]);
-
-// Queue some HTML code directly without grabbing (mostly for tests)
-
-app.listen('8081');
-exports = module.exports = app;
+        }).on('error', function(e) {
+            getNextUrl();
+        });
+    };
+}
---
+++ withoutAsync.js
@@ -0,0 +1,45 @@
+var request = require('request');
+var cheerio = require('cheerio');
+function scrapePage(arg, callback) {
+    request(arg, function(err, response, body){
+        if(body){
+            var $ = cheerio.load(body);
+            var hyperLinks = $('a');
+            for(var i=0; i<hyperLinks.length;i++){
+                console.log(items);
+                if($(hyperLinks[i]).attr('href') && items.indexOf($(hyperLinks[i]).attr('href')) !== -1){
+                    items.push($(hyperLinks[i]).attr('href'));
+                }
+            }
+            setTimeout(function() {
+                launcher();
+                callback(null);
+            }, 1000);
+        }
+    });
+    // console.log('do something with \''+arg+'\', return 1 sec later');
+}
+function final() { console.log('Done', results); }
+
+var items = [ 'https://medium.com'];
+var results = [];
+var running = 0;
+var limit = 1;
+
+function launcher() {
+    while(running < limit && items.length > 0) {
+        var item = items.shift();
+        scrapePage(item, function(result) {
+            results.push(item);
+            running--;
+            if(items.length > 0) {
+                launcher();
+            } else if(running == 0) {
+                final();
+            }
+        });
+        running++;
+    }
+}
+
+launcher();
